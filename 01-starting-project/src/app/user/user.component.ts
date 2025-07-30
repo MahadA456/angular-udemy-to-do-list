93774b302d2c6a945273a9d291cea1f5a157bc43
Mahad Alam <mahadalam3@gmail.com>
2025-07-30 20:39:23 +0500

    Implement user selection and task display features
    - Updated app.component.html to display a list of users with selection functionality.
    - Enhanced app.component.ts to manage user selection and pass the selected user's name to the tasks component.
    - Modified user.component.html and user.component.ts to handle user selection and emit events.
    - Created tasks.component.html and tasks.component.ts to display tasks based on the selected user.
    - Added basic styling for tasks in tasks.component.css.

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,7 +1,4 @@
-import { Component } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
+import { Component , Input , input , computed , Output, EventEmitter, output} from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -10,13 +7,21 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex]
-  get imagePath(){
-    return 'assets/users/'+ this.selectedUser.avatar
-  }
+  @Input({required:true}) id!: string;
+  @Input({required:true}) avatar!: string;
+  @Input({required:true}) name!: string;
+  // avatar = input.required<string>();
+  // name = input.required<string>();
+  // @Output() select = new EventEmitter();
+  select = output<string>();
 
+  // imagePath = computed(() => {
+  //   return 'assets/users/' + this.avatar();
+  // });
+  get imagePath() {
+    return 'assets/users/' + this.avatar ;
+  }
   onSelectUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
-    this.selectedUser = DUMMY_USERS[randomIndex];
+    this.select.emit(this.id);
   }
 }
